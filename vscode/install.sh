9c7d4667770e09ee89dbd967637c679bc3126113
Yuske Fukuyama <yuskefuku@gmail.com>
2020-09-06 23:07:05 +0900

    add vscode

---
+++ vscode/install.sh
@@ -0,0 +1,16 @@
+sudo xattr -dr com.apple.quarantine /Applications/Visual\ Studio\ Code.app
+
+code --install-extension tomphilbin.gruvbox-themes
+
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-python.python
+code --install-extension ms-vscode.go
+
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension jcbuisson.vue
+
+code --install-extension alexeyvax.vscode-open-native-terminal
+code --install-extension ms-azuretools.vscode-docker
+
+code --install-extension editorconfig.editorconfig
+code --install-extension vscode-icons-team.vscode-icons
